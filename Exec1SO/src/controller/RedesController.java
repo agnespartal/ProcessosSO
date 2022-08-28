294caadc219745fceb1d48427e07cd2f5b007bbe
Agnes <95083660+agnespartal@users.noreply.github.com>
2022-08-28 20:19:45 -0300

    Add files via upload

--- Exec1SO/src/controller/RedesController.java
+++ Exec1SO/src/controller/RedesController.java
@@ -11,12 +11,13 @@ public class RedesController {
 		super();
 	}
 
-	public String os() {
+	private String os() {
 		String os = System.getProperty("os.name");
 		return os;
 	}
 
-	public void ip(String so, String processo) {
+	public void ip(String processo) {
+		String so = os();
 		if (so.contains("Windows")) {
 			try {
 				Process p = Runtime.getRuntime().exec(processo);
@@ -43,12 +44,43 @@ public class RedesController {
 				
 			} catch (IOException e) {
 				e.printStackTrace();
+			} 
+				
+			} if (so.contains("Linux"));{
+				processo = "IFCONFIG";
+				try {
+					Process p = Runtime.getRuntime().exec(processo);
+					InputStream fluxo = p.getInputStream();
+					InputStreamReader leitor = new InputStreamReader(fluxo);
+					BufferedReader buffer = new BufferedReader(leitor);
+					
+					String line = buffer.readLine();
+					String titulo = "";
+					while (line != null) {
+						
+						if(line.contains("Adaptador")) {
+							titulo = line;
+						}
+						if (line.contains("IPv4")) {
+							System.out.println(titulo);
+							System.out.println(line);
+						}
+						line = buffer.readLine();
+					}
+					buffer.close();
+					leitor.close();
+					fluxo.close();
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
-		}
-		
 	}
 	
-	public void ping (String pong, String so) {
+
+
+	public void ping (String pong) {
+		String so = os();
 		if (so.contains("Windows")) {
 			try {
 				Process p = Runtime.getRuntime().exec(pong);
@@ -73,6 +105,31 @@ public class RedesController {
 			} catch(IOException e) {
 				e.printStackTrace();
 			} 		
+		} if (so.contains("Linux")) {
+			pong = "PING -4 -c 10 www.google.com.br";
+			try {
+				Process p = Runtime.getRuntime().exec(pong);
+			
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+			
+				String line = buffer.readLine();
+				while (line != null) {
+					if(line.contains("Aproximar")) {
+						line = buffer.readLine();
+						String [] linha = line.split(",");
+						System.out.println(linha[2]);
+					}
+					line = buffer.readLine();
+				}
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+			
+			} catch(IOException e) {
+				e.printStackTrace();
+			} 
 		}
 	}	
 }
